c44ce3d8ce6161708fc187989a48a9cc7f60e444
Martin Profittlich <mp2019@profittlich.com>
2024-03-03 15:23:42 -0100

    Refactored

---
+++ drawelements.py
@@ -0,0 +1,93 @@
+class CrossMarker:
+    def __init__(self, group, x1, z1, color, bold=1):
+        self.group = group
+        self.x1 = x1
+        self.z1 = z1
+        self.color = color
+        self.bold = bold
+
+class PlusMarker:
+    def __init__(self, group, x1, z1, color, bold=1):
+        self.group = group
+        self.x1 = x1
+        self.z1 = z1
+        self.color = color
+        self.bold = bold
+
+class SquareMarker:
+    def __init__(self, group, x1, z1, color, bold=1):
+        self.group = group
+        self.x1 = x1
+        self.z1 = z1
+        self.color = color
+        self.bold = bold
+
+class CircleMarker:
+    def __init__(self, group, x1, z1, color, bold=1):
+        self.group = group
+        self.x1 = x1
+        self.z1 = z1
+        self.color = color
+        self.bold = bold
+
+class UpMarker:
+    def __init__(self, group, x1, z1, color, bold=1):
+        self.group = group
+        self.x1 = x1
+        self.z1 = z1
+        self.color = color
+        self.bold = bold
+
+class DownMarker:
+    def __init__(self, group, x1, z1, color, bold=1):
+        self.group = group
+        self.x1 = x1
+        self.z1 = z1
+        self.color = color
+        self.bold = bold
+
+class LeftLineMarker:
+    def __init__(self, group, x1, z1, x2, z2, color, bold=1, length=100, endText = None):
+        self.group = group
+        self.x1 = x1
+        self.z1 = z1
+        self.x2 = x2
+        self.z2 = z2
+        self.color = color
+        self.bold = bold
+        self.length = length
+        self.endText = endText
+
+class Text:
+    def __init__(self, group, x1, z1, text, offsetx, offsetz, color, bold=1):
+        self.group = group
+        self.x1 = x1
+        self.z1 = z1
+        self.offsetx = offsetx
+        self.offsetz = offsetz
+        self.text = text
+        self.color = color
+        self.bold = bold
+
+class Line:
+    def __init__(self, group, x1, z1, x2, z2, color, bold=1):
+        self.group = group
+        self.x1 = x1
+        self.z1 = z1
+        self.x2 = x2
+        self.z2 = z2
+        self.color = color
+        self.bold = bold
+
+class Triangle:
+    def __init__(self, group, x1, z1, x2, z2, x3, z3, color):
+        self.group = group
+        self.x1 = x1
+        self.z1 = z1
+        self.x2 = x2
+        self.z2 = z2
+        self.x3 = x3
+        self.z3 = z3
+        self.color = color
+
+
--- visuallapcompare.py
+++ visuallapcompare.py
@@ -21,6 +21,7 @@ from helpers import Lap, PositionPoint
 
 import gt7telemetryreceiver as tele
 from gt7widgets import *
+from drawelements import *
 
 class StartWindowVLC(QWidget):
     def __init__(self):
@@ -79,98 +80,6 @@ class StartWindowVLC(QWidget):
             for l in self.bLaps:
                 self.idxRefB.addItem(str(l.points[0].current_lap) + ": " + str(indexToTime(len(l.points))))
 
-
-class CrossMarker:
-    def __init__(self, group, x1, z1, color, bold=1):
-        self.group = group
-        self.x1 = x1
-        self.z1 = z1
-        self.color = color
-        self.bold = bold
-
-class PlusMarker:
-    def __init__(self, group, x1, z1, color, bold=1):
-        self.group = group
-        self.x1 = x1
-        self.z1 = z1
-        self.color = color
-        self.bold = bold
-
-class SquareMarker:
-    def __init__(self, group, x1, z1, color, bold=1):
-        self.group = group
-        self.x1 = x1
-        self.z1 = z1
-        self.color = color
-        self.bold = bold
-
-class CircleMarker:
-    def __init__(self, group, x1, z1, color, bold=1):
-        self.group = group
-        self.x1 = x1
-        self.z1 = z1
-        self.color = color
-        self.bold = bold
-
-class UpMarker:
-    def __init__(self, group, x1, z1, color, bold=1):
-        self.group = group
-        self.x1 = x1
-        self.z1 = z1
-        self.color = color
-        self.bold = bold
-
-class DownMarker:
-    def __init__(self, group, x1, z1, color, bold=1):
-        self.group = group
-        self.x1 = x1
-        self.z1 = z1
-        self.color = color
-        self.bold = bold
-
-class LeftLineMarker:
-    def __init__(self, group, x1, z1, x2, z2, color, bold=1, length=100, endText = None):
-        self.group = group
-        self.x1 = x1
-        self.z1 = z1
-        self.x2 = x2
-        self.z2 = z2
-        self.color = color
-        self.bold = bold
-        self.length = length
-        self.endText = endText
-
-class Text:
-    def __init__(self, group, x1, z1, text, offsetx, offsetz, color, bold=1):
-        self.group = group
-        self.x1 = x1
-        self.z1 = z1
-        self.offsetx = offsetx
-        self.offsetz = offsetz
-        self.text = text
-        self.color = color
-        self.bold = bold
-
-class Line:
-    def __init__(self, group, x1, z1, x2, z2, color, bold=1):
-        self.group = group
-        self.x1 = x1
-        self.z1 = z1
-        self.x2 = x2
-        self.z2 = z2
-        self.color = color
-        self.bold = bold
-
-class Triangle:
-    def __init__(self, group, x1, z1, x2, z2, x3, z3, color):
-        self.group = group
-        self.x1 = x1
-        self.z1 = z1
-        self.x2 = x2
-        self.z2 = z2
-        self.x3 = x3
-        self.z3 = z3
-        self.color = color
 
 class MapView2(QWidget):
 
