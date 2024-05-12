141ed9d23cb3e1171da9047e8366df7fb100697c
Martin Profittlich <mp2019@profittlich.com>
2024-05-12 21:03:59 +0200

    Create laptotrack.py

---
+++ laptotrack.py
@@ -0,0 +1,30 @@
+import sys
+from sb.gt7telepoint import Point
+from sb.helpers import loadLap
+from sb.helpers import Lap
+import copy
+
+def thinOut (fni, fno):
+    sep = ";"
+    lap = loadLap(fni)
+    lap2 = Lap()
+    pOld = lap.points[0]
+    lap2.points.append(pOld)
+    for p in range(1, len(lap.points)):
+        pNew = lap.points[p]
+
+        if lap.distance(pOld, pNew) > 50 or lap.angle(pOld, pNew) > 3.14159 * 10 / 180:
+            lap2.points.append(pNew)
+            print("Angle:", 180 * lap2.angle(pOld, pNew)/ 3.14159)
+            pOld = pNew
+    
+    with open ( fno, "wb") as f:
+        for p in lap2.points:
+            f.write(p.raw)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("usage: laptotrack <lapfile> <trackfile>")
+    else:
+        thinOut(sys.argv[1], sys.argv[2])
+
